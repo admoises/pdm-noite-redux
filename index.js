bb2c2bde84490f7b78fc94d2c679fe75ee8a3ca3
Alec Douglas Moisés <alec.moises@outlook.com>
2022-05-20 21:42:26 -0300

    Implementação dos reducers

--- index.js
+++ index.js
@@ -30,3 +30,41 @@ const solicitarCashBack = (nome, valor) => {
         payload: {nome, valor}
     }
 }
+
+//arrow function
+//historicoDePedidosCashbackReducer
+//recebe uma fatia de estado sob o qual operar (uma lista chamada historicoDePedidosCashback, por padrão ela é uma lista vazia)
+//recebe uma ação
+//devolve uma lista que contém todos os pedidos da lista recebido e o pedido descrito no payload da ação
+//detalhe: somente operar o estado se o type for apropriado
+//detalhe2: obrigatório usar o spread (...)
+
+
+const historicoDePedidosCashbackReducer = (historicoDePedidosCashback = [], acao) => {
+    return acao.type === "SOLICITAR_CASHBACK" ? [...historicoDePedidosCashback, acao.payload] : historicoDePedidosCashback
+}
+
+//implementar reducer que opera o caixa
+
+const operaCaixaReducer = (caixa = 0, acao) => {
+    if (acao.type === "CRIAR_CONTRATO") {
+        return caixa + acao.payload.valor;
+    }
+    else if (acao.type === "SOLICITAR_CASHBACK") {
+        return caixa - acao.payload.valor;
+    }
+    return valor;
+}
+
+//implementar reducer que lida com alista de contratos
+//ele pode criar contratos e cancelar contrator
+
+const contratosReducer = (listaDeContratosAtual = [], acao) => {
+    if (acao.type === "CRIAR_CONTRATO") {
+        return [...listaDeContratosAtual, acao.payload]
+    }
+    if ( acao.type === "CANCELAR_CONTRATO") {
+        return listaDeContratosAtual.filter((contrato) => {contrato.nome !== acao.payload.nome})
+    }
+    return listaDeContratosAtual;
+}
